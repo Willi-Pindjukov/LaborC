5899cd8fdd38a623da7ae19fd6e3150d1bfdb719
Willi <80400277+Willi-Pindjukov@users.noreply.github.com>
2022-06-22 19:35:16 +0200

    add files for 3rd Meeting
    add files for 3rd Meeting

--- followLine.c
+++ followLine.c
@@ -1,9 +1,47 @@
-#include "iesmotors.h"
 #include <util/delay.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include "iesmotors.h"
+#include "iesusart.h"
+#include "iesadc.h"
+
+#define VALUERIGHT 600;
+#define VALUEMIDDLE 500;
+#define VALUELEFT 500;
+
+typedef enum State {right, left, middle, middleright, middleleft} State;
+
+
+
+void drivemiddle(State* state){
+	setDutyCycle(PD5, 100);
+    setDutyCycle(PD6, 100);
+	PORTB |= (1 << PB3);
+	PORTD |= (1 << PD7);
+	*state = middle;
+	}
+
+void driveright(State* state){
+	setDutyCycle(PD5, 155);
+    setDutyCycle(PD6, 155);
+	PORTD |= (1 << PD7);
+	PORTB &= ~(1 << PB3);
+	*state = right;
+	}
+	
+void driveleft(State* state){
+	setDutyCycle(PD5, 155);
+    setDutyCycle(PD6, 155);
+	PORTB |= (1 << PB3);
+	PORTD &= ~(1 << PD7);
+	*state = left;
+	}
+
 
 
 int main(void) { 
 	
+	USART_init(UBRR_SETTING);
 	// Set Data Direction Register C [0|1|2| as input.
     DDRC &= ~((1<<DDC0) | (1<<DDC1) | (1<<DDC2));
     //USART_init(UBRR_SETTING);
@@ -22,7 +60,7 @@ int main(void) {
     setupTimer0();
 
     // Set PB0, PB1, and PB3 as output (IN[2|3|4])
-    DDRB = (1 << DD0) | (1 << DD1) | (1 << DD3);
+    DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB3);
 
     // Set the duty cycles for PD5/PD6
     setDutyCycle(PD5, 155);
@@ -31,111 +69,108 @@ int main(void) {
 	// set backword motors on 0 for safety
 	PORTB &= ~(1 << PB0);
 	PORTB &= ~(1 << PB1);
+	
+	char right;
+	char middle;
+	char left;
 
 	// drive with both motors forward (till middle sensor is 0)
-	while(1) {
-		while(PINC & (1 << PINC1)){
+	/*while(1) {
+		
+		char middle = (PINC & (1 << PINC1));
+		char right = (PINC & (1 << PINC2));
+		char left = (PINC & (1 << PINC0));
+		char middleleft = (PINC & (1 << PINC1)) && (PINC & (1 << PINC0));
+		char middleright = (PINC & (1 << PINC1)) && (PINC & (1 << PINC2));
+		
+		while(middle){
+			USART_print("Middle sensor\n");
 			//Right motor on forward
 			PORTB |= (1 << PB3);
 			//Left motor on forward    
 			PORTD |= (1 << PD7);
-			
-			
-			
-			//if middle and right sonsor are 1
-			if((PINC & (1 << PINC1)) && (PINC & (1 << PINC0))){
-				
-				//Right motor off forward
+			if(PINC & ~(1 << PINC1)){
 				PORTB &= ~(1 << PB3);
-				//Left motor off forward
 				PORTD &= ~(1 << PD7);
-				//_delay_ms(3000);
-				while(PINC & (1 << PINC0)){
-					PORTB |= (1 << PB3);
-					}
+				break;
 				}
-			
-			//if middle and left sonsor are 1
-			if((PINC & (1 << PINC1)) && (PINC & (1 << PINC2))){
-				
-				//Right motor off forward
-				PORTB &= ~(1 << PB3);
-				//Left motor off forward
-				PORTD &= ~(1 << PD7);
-				//_delay_ms(3000);
-		
-				while(PINC & (1 << PINC2)){
-					PORTD |= (1 << PD7);
-					}
-				}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			// if right sensor 1 -> start right motor forward (till right sensor is 0)
-			if(PINC & (1 << PINC0)){
-				
-				//Right motor off forward
-				PORTB &= ~(1 << PB3);
-				//Left motor off forward
-				PORTD &= ~(1 << PD7);
-				//_delay_ms(3000);
-				while(PINC & (1 << PINC0)){
-					PORTB |= (1 << PB3);
-					}
-				}
-			// if left sensor 1 -> start left motor forward (till left sensor is 0)
-			if(PINC & (1 << PINC2)){
-				
-				//Right motor off forward
-				PORTB &= ~(1 << PB3);
-				//Left motor off forward
-				PORTD &= ~(1 << PD7);
-				//_delay_ms(3000);
-		
-				while(PINC & (1 << PINC2)){
-					PORTD |= (1 << PD7);
-					}
-				}
-			
 		}
 		
-		// if right sensor 1 -> start right motor forward (till right sensor is 0)
-		if(PINC & (1 << PINC0)){
-			
-			//Right motor off forward
-			PORTB &= ~(1 << PB3);
-			//Left motor off forward
-			PORTD &= ~(1 << PD7);
-			//_delay_ms(3000);
-			while(PINC & (1 << PINC0)){
-				PORTB |= (1 << PB3);
-				}
-			}
-		// if left sensor 1 -> start left motor forward (till left sensor is 0)
-		if(PINC & (1 << PINC2)){
-			
-			//Right motor off forward
-			PORTB &= ~(1 << PB3);
-			//Left motor off forward
-			PORTD &= ~(1 << PD7);
-			//_delay_ms(3000);
 	
-			while(PINC & (1 << PINC2)){
-				PORTD |= (1 << PD7);
+		while(left) {
+			USART_print("Left sensor\n");
+			PORTB |= (1 << PB3);
+			if(PINC & ~(1 << PINC0)){
+				PORTB &= ~(1 << PB3);
+				break;
 				}
 			}
+	
+	
+		while(right) {
+			USART_print("Right sensor\n");
+			PORTD |= (1 << PD7);
+			if(PINC & ~(1 << PINC2)){
+				PORTD &= ~(1 << PD7);
+				break;
+				}
+			}
+*/
+				
 		
-	}
+		USART_init(UBRR_SETTING);
+
+		DR_ADC0 &= ~(1 << DP_ADC0);
+		DR_ADC1 &= ~(1 << DP_ADC1);
+		DR_ADC2 &= ~(1 << DP_ADC2);
+
+		ADC_init();
+
+		unsigned char strbuff[sizeof(ADCMSG) + 15]; // WTF, why + 15? Oo
+
+		// Just to make things clear: You have to be extremely careful with
+		// the size of the stringbuffer. Better safe than sorry! But memory
+		// as well as time are so so so precious!
+
+		uint16_t adcval0 = 0;
+		uint16_t adcval1 = 0;
+		uint16_t adcval2 = 0;
+		
+		State CurrentState;
+		while(1) {
+			
+		
+			adcval0 = ADC_read_avg(ADMUX_CHN_ADC0, ADC_AVG_WINDOW);
+			adcval1 = ADC_read_avg(ADMUX_CHN_ADC1, ADC_AVG_WINDOW);
+			adcval2 = ADC_read_avg(ADMUX_CHN_ADC2, ADC_AVG_WINDOW);
+			
+			right = ADC_read_avg(ADMUX_CHN_ADC0, ADC_AVG_WINDOW) > VALUERIGHT;
+			middle = ADC_read_avg(ADMUX_CHN_ADC1, ADC_AVG_WINDOW) > VALUEMIDDLE;
+			left = ADC_read_avg(ADMUX_CHN_ADC2, ADC_AVG_WINDOW) > VALUELEFT;
+			
+			if(middle){
+				drivemiddle(&CurrentState);
+			}else if(right){
+				driveright(&CurrentState);
+			}else if(left){
+				driveleft(&CurrentState);
+			}else if(CurrentState == middle){
+				drivemiddle(&CurrentState);
+			}else if(CurrentState == right){
+				driveright(&CurrentState);
+			}else if(CurrentState == left){
+				driveleft(&CurrentState);
+			}
+			
+				
+			sprintf(strbuff, ADCMSG, adcval0, adcval1, adcval2);
+
+			USART_print(strbuff);
+		}
+
+
+		
+		_delay_ms(1000);
+	
 	return 0;
-// Fehler/Problem: Es läuft eine Seite immer weiter, obwohl nichts dadrunter ist
 }
---
+++ iesadc.h
@@ -0,0 +1,42 @@
+
+
+#ifndef IESADC_h
+#define IESADC_h
+
+// Average some measurements to reduce probable noise.
+#define ADC_AVG_WINDOW 20
+
+// Well hmmm understand this by yourself. Become inspired.
+#define ADCMSG "ADC0: %5u\tADC1: %5u\tADC2: %5u\n"
+
+// WTF. Why is there no header here? Always seperate declaration,
+// documentation, and implementation! Clean this up if you use this!
+
+#define DR_ADC0 DDRC
+#define DP_ADC0 DDC0
+#define IR_ADC0 PC0
+#define IP_ADC0 PC0
+
+#define DR_ADC1 DDRC
+#define DP_ADC1 DDC1
+#define IR_ADC1 PC1
+#define IP_ADC1 PC1
+
+#define DR_ADC2 DDRC
+#define DP_ADC2 DDC2
+#define IR_ADC2 PC2
+#define IP_ADC2 PC2
+
+// The following constants reflect table 23-4 of the ATMega328p-data-
+// sheet. These constants
+#define ADMUX_CHN_ADC0 0 // 0000 0000
+#define ADMUX_CHN_ADC1 1 // 0000 0001
+#define ADMUX_CHN_ADC2 2 // 0000 0010
+#define ADMUX_CHN_ADC3 3 // 0000 0011
+#define ADMUX_CHN_ALL 7 // 0000 1111
+
+
+void ADC_init(void);
+
+uint16_t ADC_read_avg(uint8_t channel, uint8_t nsamples);
+#endif
